74089978123e73cd95a125cf4a608c45f4586678
0xJayPi <jpcampaya@gmail.com>
2022-12-26 11:30:10 +0100

    Games completed

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,35 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { Signer } = require("ethers");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
+    const [signer] = await ethers.getSigners(0);
+    let wallet;
 
-    // good luck
+    while (true) {
+      wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      if (wallet.address < 0x00ffffffffffffffffffffffffffffffffffffff) {
+        break;
+      }
+    }
 
-    await game.win();
+    console.log(`Address found, ${wallet.address}`);
+    const to = wallet.address;
+    const value = ethers.utils.parseEther("1");
 
-    // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    // wallet has no funds to pay for gas
+    await signer.sendTransaction({ to, value });
+    await game.connect(wallet).win();
+
+    assert(await game.isWon(), "You did not win the game");
   });
 });
